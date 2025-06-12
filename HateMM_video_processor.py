1e846d8e1d2e0e420ac25485400619529abb9302
Jack <40670088+JDonohoe101@users.noreply.github.com>
2025-06-12 21:38:24 +0100

    init

---
+++ HateMM_video_processor.py
@@ -0,0 +1,60 @@
+# -- Imports --
+import os
+import HateMM_util_functions as utils
+from rich.progress import Progress, track
+import time
+
+#TODO: extract validation labels from each video and attribute them in the dictionary
+#TODO: Instead of exporting to folders, export as PROMPTS
+
+def process_videos(PATH: str) -> None:
+    """
+    This is the main function which extracts multimodal semantic features from
+    videos in to prompts that can be fed in to both open and closed
+    source LLMs.
+    """    
+
+    total_extracted_video_data_list = [] #list to hold all videos extracted data
+    VALID_EXTENSIONS = ['.mp4', '.mov', '.avi', '.mkv'] #checking formats avoids trying to process hidden files
+
+    #Counts number of videos in a folder to inform the progress bar total
+    video_count=0 #counter variable
+    for video in os.scandir(PATH):
+            if video.is_file() and os.path.splitext(video.name)[1].lower() in VALID_EXTENSIONS:
+                video_count += 1
+
+    # -- Progress bar wrapper --
+    with Progress() as progress:
+        task = progress.add_task("[green]Processing videos...", total=video_count)
+
+        # -- Main loop --
+        for video in os.scandir(PATH):
+            if video.is_file() and os.path.splitext(video.name)[1].lower() in VALID_EXTENSIONS:  # check if it's a (valid) video file
+                video_name = video.name
+                progress.console.log(f"Extracting data from {video_name}", style='bold magenta')
+
+                # -- Calls to the extraction logic in util file --
+                video_frames = utils.get_video_frames(video_name)
+                video_audio_transcript = utils.get_audio_transcript(video_name)
+                print(video_audio_transcript)#debug statement, remove later
+                video_visual_description = utils.get_visual_description(video_name)
+
+                extracted_video_data = { #Dictionary used to hold all extracted data in a video
+                    "video_name" : video_name,
+                    "video_frames" : video_frames,
+                    "video_audio_transcript" : video_audio_transcript,
+                    "video_visual_description" : video_visual_description
+                }
+
+                total_extracted_video_data_list.append(extracted_video_data)
+                progress.update(task, advance=1) #advances progress bar by one step
+                #time.sleep(0.01)  #debug statement for progress bar
+
+        utils.export_to_folder(total_extracted_video_data_list) #call to export logic
+
+# -- Initial logic --
+if __name__ == "__main__":
+    #Defines the path to the video folder and update the path in the utils module.
+    PATH = "/Users/jackdonohoe/MSC RESEARCH PROJECT/Datasets/HateMM - Data/7799469/func_test"
+    utils.current_path=PATH
+    process_videos(PATH)
